f9ce941fa300c22bb17f55452d826e346075df9f
Medina Softic <medinasoftic@Mac.attlocal.net>
2025-03-24 18:18:57 -0500

    completed setting up the page

---
+++ index.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("input")
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+
+function check() {
+    const value = input.value
+    alert(value)
+
+
+}
